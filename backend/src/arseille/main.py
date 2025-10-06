2bfe9a67ddcd2201497fadd2146404d34a7bfbc1
jfpiedad <jfpiedad@gmail.com>
2025-10-06 12:02:47 +0800

    basic hello world on both frontend and backend

--- backend/src/arseille/main.py
+++ backend/src/arseille/main.py
@@ -1,3 +1,11 @@
+from typing import Any
+
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
 
 
-
+@app.get("/", response_class=PlainTextResponse)
+async def root() -> Any:
+    return "Hello World"
