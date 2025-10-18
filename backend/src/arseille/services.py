2b941db5980b459e64a5c4fe06e987a03c6e1dfc
jfpiedad <jfpiedad@gmail.com>
2025-10-18 14:24:52 +0800

    added service for getting weather data through an API

---
+++ backend/src/arseille/services.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+
+import httpx
+from cachetools.func import ttl_cache
+from httpx import HTTPError
+
+from arseille.config import settings
+from arseille.enums import Weather
+
+
+@ttl_cache(maxsize=128, ttl=timedelta(minutes=15).seconds)
+def get_current_weather() -> Weather:
+    url = settings.WEATHER_API_BASE_URL
+
+    query_params = {
+        "q": "10.293893, 123.897487",
+        "key": settings.WEATHER_API_KEY,
+    }
+
+    temperature = 25
+
+    try:
+        response = httpx.get(url=f"{url}/current.json", params=query_params)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            data = response.json()
+            temperature = data["current"]["temp_c"]
+    except HTTPError:
+        print("Cannot get weather data.")
+
+    if temperature < 20:
+        weather = Weather.COLD
+    elif 20 <= temperature <= 30:
+        weather = Weather.MODERATE
+    else:
+        weather = Weather.HOT
+
+    return weather
+
+
+if __name__ == "__main__":
+    for _ in range(5):
+        get_current_weather()
